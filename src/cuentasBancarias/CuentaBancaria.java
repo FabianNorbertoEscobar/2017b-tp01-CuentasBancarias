f7a084768849dc5d50275eece16a2c715994ab75
Asd <amengualma6@gmail.com>
2017-09-08 14:05:20 -0300

    VERDE: se crea constructor sin parámetros --> se puede construir cuenta sin parámetro

--- src/cuentasBancarias/CuentaBancaria.java
+++ src/cuentasBancarias/CuentaBancaria.java
@@ -3,7 +3,11 @@ package cuentasBancarias;
 public class CuentaBancaria {
 
 	private double saldo;
-	
+
+	public CuentaBancaria() {
+		this.saldo = 0;
+	}
+
 	public CuentaBancaria(double saldo) {
 		this.saldo = saldo;
 	}
